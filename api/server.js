be5e7f83cf29e2f231bcdb7fbf3b6d2d0eb177b3
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-05-02 16:51:48 -0400

    Server started and working

--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
 // implement your server here
+const express = require('express');
+const postsRouter = require('./posts/posts-router')
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/posts', postsRouter)
+
 // require your posts router and connect it here
+server.use('*', (req,res) => {
+    res.status(404).json({
+        message:' not found'
+    })
+})
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+const server = require('./api/server')
+
+const Port = 9000
+
+server.listen(Port, () => {
+    console.log(`Server running on http://localhost:${Port}`)
+})
+
+module.exports = server;
